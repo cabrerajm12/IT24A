4c8c2fd1aa8a30f5eabe4a6ea4461f40b384608e
cabrerajm12 <josemariacabrera720@gmail.com>
2024-09-26 10:43:08 +0800

    added conditional statements

--- app.js
+++ app.js
@@ -43,3 +43,49 @@ console.log(result1);
 console.log(result2);
 console.log(result3);
 console.log(result4);
+
+//C. Condition statements
+
+// if statement
+let tomorrow = new Date().getDate();
+let greetings, greet;
+if(tomorrow < 18){
+greetings = "Have a nice day";
+}
+    console.log(greetings);
+
+    //if else statement
+    if (tomorrow > 15){
+        greet = "good morning";
+    }
+    else{
+        greet = "you too";
+    }
+    console.log(greet);
+
+    // else if statement
+    if (tomorrow > 15){
+        variable = "hi guys";
+    }else if(tomorrow < 15){
+        variable = "welcome to my Github";
+    }else{
+        variable ="have a nice look";
+    }
+    console.log(variable);
+
+    //switch statement
+let week = 3;
+let greeting;
+
+switch(week){
+    case 1:
+        greeting = "hi guys welcome to my Github";
+        break;
+    case 2:
+        greeting = "today we going to do the activity";
+        break;
+    case 3:
+        greeting = "were going to finish the activity";
+        break;    
+}
+console.log(greeting);
