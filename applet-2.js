262003259d03fd4ce56a32666094dc795008b6e4
cabrerajm12 <josemariacabrera720@gmail.com>
2024-09-30 15:02:25 +0800

    added initTileLayer method

--- applet-2.js
+++ applet-2.js
@@ -1,5 +1,11 @@
 class leafLetMap{
     constructor(containerId, center, zoom){
       this.map = L.map(containerId).setView(center, zoom);
+      this.initTileLayer();
+    }
+    initTileLayer(){
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | sample in NBSC'
+        }).addTo(this.map);
     }
 }
