105c777855cfc52b0075b4bfa6dc0218bf56a55b
cabrerajm12 <josemariacabrera720@gmail.com>
2024-09-30 15:12:29 +0800

    added loadMarkersFromJson method

--- applet-2.js
+++ applet-2.js
@@ -14,6 +14,16 @@ class leafLetMap{
         .bindPopup(message)
     }
 
+    loadMarkersFromJson(url){
+        fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(marker => {
+                this.addMarker(marker.latitude, marker.longitude, marker.message);
+            });
+        })
+        .catch(error => console.error("Error Loading servers:", error));
+    }
 }
 
 const Mymap = new leafLetMap('map', [8.359735, 124.869206], 18);
