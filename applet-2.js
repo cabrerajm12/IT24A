805e7d942cab44aaa3e94b680c07c7d619b35283
cabrerajm12 <josemariacabrera720@gmail.com>
2024-09-30 15:16:04 +0800

    added obejct loadMarkersFromJson in applet-2.js

--- applet-2.js
+++ applet-2.js
@@ -31,3 +31,6 @@ const Mymap = new leafLetMap('map', [8.359735, 124.869206], 18);
 Mymap.addMarker(8.359735, 124.869206, 'CCS Faculty Office');
 Mymap.addMarker(8.359639, 124.869179, 'CCS Laboratory 1');
 Mymap.addMarker(8.359554, 124.869153, 'CCS Laboratory 2');
+
+
+Mymap.loadMarkersFromJson('applet-2.json');
