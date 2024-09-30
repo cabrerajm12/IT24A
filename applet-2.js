70118dc6421ffcf9305378a77c0c0413af192324
cabrerajm12 <josemariacabrera720@gmail.com>
2024-09-30 15:09:24 +0800

    added addMarker method

--- applet-2.js
+++ applet-2.js
@@ -9,6 +9,11 @@ class leafLetMap{
         }).addTo(this.map);
     }
 
+    addMarker(lat,long, message){
+      const marker =  L.marker([lat, long]).addTo(this.map)
+        .bindPopup(message)
+    }
+
 }
 
 const Mymap = new leafLetMap('map', [8.359735, 124.869206], 18);
