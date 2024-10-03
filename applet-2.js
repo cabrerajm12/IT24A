22b184460fc07a76d665d1f03e058a02ac5840b8
cabrerajm12 <josemariacabrera720@gmail.com>
2024-10-03 10:15:33 +0800

    added event listeners for the buttons

--- applet-2.js
+++ applet-2.js
@@ -8,6 +8,10 @@ class leafLetMap{
         this.logCount1Element = document.getElementById('logCountBA');
         this.logCount2Element = document.getElementById('logCountCCS');
         this.idContainer = document.getElementById('logContainer');
+        this.btn.addEventListener('click', () => this.dataSc());
+        this.btn1.addEventListener('click', () => this.dataLab());
+        this.btn2.addEventListener('click', () => this.dataBa());
+        this.btnclear.addEventListener('click', () => this.clearLogs());
     }
     initTileLayer(){
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
