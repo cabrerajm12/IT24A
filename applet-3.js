91d19d7c218aef1c67d253f97cf94197307ee246
cabrerajm12 <josemariacabrera720@gmail.com>
2024-10-03 09:46:50 +0800

    added loggData object

--- applet-3.js
+++ applet-3.js
@@ -1,10 +1,11 @@
 class dataLogger{
-    
+
     constructor(buttonId, cardContainerId, clearButtonId, logCountId){
         this.logButton = document.getElementById(buttonId);
         this.clearButton = document.getElementById(clearButtonId);
         this.idContainer = document.getElementById(cardContainerId);
         this.logCountElement = document.getElementById(logCountId);
+        this.loggedData = [];
 
     }
 }
