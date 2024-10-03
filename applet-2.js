5d441c0a224db4091901d5507d078b65b0c4a54e
cabrerajm12 <josemariacabrera720@gmail.com>
2024-10-03 10:29:20 +0800

    added DOMContentLoaded

--- applet-2.js
+++ applet-2.js
@@ -108,6 +108,9 @@ class leafLetMap{
 
 const Mymap = new leafLetMap('map', [8.359735, 124.869206], 18);
 
-
-
 Mymap.loadMarkersFromJson('applet-2.json');
+
+document.addEventListener('DOMContentLoaded', () => {
+    Mymap.displayLogCount();
+    Mymap.loadMarkersFromJson('applet-2.json');
+});
