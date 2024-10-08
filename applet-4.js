d50dd1d6ba31f161bdfd6e31f4ddf6928dd39cec
cabrerajm12 <josemariacabrera720@gmail.com>
2024-10-08 14:05:18 +0800

    added init method

--- applet-4.js
+++ applet-4.js
@@ -2,6 +2,11 @@ class StudentList {
   constructor(dataUrl) {
       this.dataUrl = dataUrl;
       this.students = [];
+      this.init();
   }
-
+    async init() {
+        await this.fetchData();
+        this.renderStudentList(this.students); 
+        this.bindSearchEvent();
+    }
 }
