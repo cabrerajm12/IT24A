457b2d825b19bdad2fe5f4594d898f96ebce8056
cabrerajm12 <josemariacabrera720@gmail.com>
2024-10-09 19:24:21 +0700

    added constructor

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 class searchCard {
-    
+        constructor(searchInputId, cardContainerClass) {
+          this.searchInput = document.getElementById(searchInputId);
+          this.cardContainer = document.querySelector(`.${cardContainerClass}`);
+          this.cards = this.cardContainer.querySelectorAll('.card');
+    }
 }
