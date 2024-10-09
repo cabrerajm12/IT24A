5ae5174144299fb2f0fe4c9159e2a6e407a9ac18
cabrerajm12 <josemariacabrera720@gmail.com>
2024-10-09 19:25:11 +0700

    added init method

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ class searchCard {
           this.searchInput = document.getElementById(searchInputId);
           this.cardContainer = document.querySelector(`.${cardContainerClass}`);
           this.cards = this.cardContainer.querySelectorAll('.card');
+          this.init();
     }
+    init() {
+        this.searchInput.addEventListener('input', () => {
+            this.filterCards(this.searchInput.value);
+        });
+}
 }
