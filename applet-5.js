710026cd72e61b4d3161259b7b79d9f20ed4b9d2
cabrerajm12 <josemariacabrera720@gmail.com>
2024-10-16 13:50:47 +0700

    added getWeatherData method

--- applet-5.js
+++ applet-5.js
@@ -76,4 +76,17 @@ class WeatherService extends weatherApp {
             alert('Geolocation is not supported by this browser.');
         }
     }
+
+    async getWeatherData(city) {
+        try {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apiKey}&units=metric`);
+            if (response.ok) {
+                return await response.json();
+            }
+        } catch (error) {
+            console.error('Error fetching weather data:', error);
+        }
+        return null;
+    }
+
 }
