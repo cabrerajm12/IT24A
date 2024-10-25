09d30a2db283ae74f027bf95cfa373c9d66074ce
cabrerajm12 <josemariacabrera720@gmail.com>
2024-10-25 09:23:13 +0800

    added removeTask method

--- applet-6.js
+++ applet-6.js
@@ -47,4 +47,7 @@ class todoList{
         this.todoList.children[this.editingIndex].querySelector('.task-text').textContent = taskText;
         this.resetEditing();
     }
+    removeTask(event) {
+        this.todoList.removeChild(event.target.closest('.todo-item'));
+    }
 }
